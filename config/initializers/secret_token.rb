2c8da6fb07a1467b1efa8a3cf138a1247f12a2c1
Greg McClure <gmcclure@gmail.com>
2013-06-27 22:43:57 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Annobiblio::Application.config.secret_key_base = 'a53cee8f05bf5278da4998811042683e93b104fb770ed02bb1b664c7fb7574fd4779c377edcacb34fe948f168f51730abbba0303d7ec235740268b6b62591551'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Annobiblio::Application.config.session_store :cookie_store, key: '_annobiblio_session'
