4be154aae73a07804dd37783c7a81571305e87dd
Greg McClure <gmcclure@gmail.com>
2013-06-28 23:32:22 -0700

    Can add courses.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,15 @@
 class Course < ActiveRecord::Base
+  validates :title, presence: true
+  validates :password, presence: true
+
+  after_create do |course|
+    course.code = create_course_code(course.id)
+    course.save
+  end
+
+  private
+
+  def create_course_code(id)
+    (id + Settings.course_seed).to_s(36)
+  end
 end
